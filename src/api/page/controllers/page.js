eba785bf885e8cd0221caa928db1a182a3bcb932
João P M Morais <paulomedeiros5000@gmail.com>
2022-04-27 20:05:27 -0300

    create components and collections

--- src/api/page/controllers/page.js
+++ src/api/page/controllers/page.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- *  post controller
+ *  page controller
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::post.post');
+module.exports = createCoreController('api::page.page');
--- src/api/page/services/page.js
+++ src/api/page/services/page.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * post service.
+ * page service.
  */
 
 const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService('api::post.post');
+module.exports = createCoreService('api::page.page');
