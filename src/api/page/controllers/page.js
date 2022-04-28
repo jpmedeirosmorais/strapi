ad8da79d331634f227734b9e79cc878044d91e07
João P M Morais <paulomedeiros5000@gmail.com>
2022-04-28 13:27:43 -0300

    public api

--- src/api/page/controllers/page.js
+++ src/api/page/controllers/page.js
@@ -1,9 +1,64 @@
-'use strict';
+const { createCoreController } = require("@strapi/strapi").factories;
 
-/**
- *  page controller
- */
+module.exports = createCoreController("api::page.page", ({ strapi }) => ({
+  async find(ctx) {
+    const { query } = ctx;
 
-const { createCoreController } = require('@strapi/strapi').factories;
+    const entity = await strapi.entityService.findMany("api::page.page", {
+      ...query,
+      populate: {
+        menu: {
+          populate: {
+            logo: true,
+            logo_text: true,
+            menu_link: {
+              populate: {
+                link_text: true,
+                url: true,
+              },
+            },
+          },
+        },
+        sections: {
+          populate: {
+            title: true,
+            content: true,
+            description: true,
+            image: true,
+            text_grid: {
+              populate: {
+                title: true,
+                description: true,
+              },
+            },
+            image_grid: {
+              populate: {
+                image: true,
+              },
+            },
+            metadata: {
+              populate: {
+                name: true,
+                section_id: true,
+                background: true,
+              },
+            },
+          },
+        },
+        sidebar: {
+          populate: {
+            img: true,
+            links: {
+              populate: {
+                icon: true,
+              },
+            },
+          },
+        },
+      },
+    });
+    const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
 
-module.exports = createCoreController('api::page.page');
+    return this.transformResponse(sanitizedEntity);
+  },
+}));
